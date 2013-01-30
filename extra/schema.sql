2c67cd59f3b8a0c1f3473a3eca90f37d3eb79fa1
Keith Gaughan <k@stereochro.me>
2013-01-30 16:44:33 +0000

    Trim back the schema to the bare minimum.

--- extra/schema.sql
+++ extra/schema.sql
@@ -1,24 +1,9 @@
 CREATE TABLE pastes (
 	paste_id    INTEGER  NOT NULL PRIMARY KEY,
 	created     DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	name        TEXT     NOT NULL,
-	description TEXT     NOT NULL,
-	body        TEXT     NOT NULL
+	poster      TEXT     NOT NULL DEFAULT 'Anonymous',
+	body        TEXT     NOT NULL,
+	format      TEXT     NOT NULL DEFAULT 'text'
 );
 
 CREATE INDEX ix_paste_created ON pastes (created);
-
-CREATE TABLE comments (
-	comment_id INTEGER  NOT NULL PRIMARY KEY,
-	paste_id   INTEGER  NOT NULL DEFAULT 0 REFERENCES pastes (paste_id),
-	created    DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	comment    TEXT     NOT NULL
-);
-
-CREATE TABLE users (
-	user_id  INTEGER NOT NULL PRIMARY KEY,
-	username TEXT    NOT NULL,
-	email    TEXT    NULL
-);
-
-CREATE UNIQUE INDEX ux_user_username ON users (username);
