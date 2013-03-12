d14361c1c5641a58c05d81665c34509c686db989
Keith Gaughan <k@stereochro.me>
2013-03-12 13:08:25 +0000

    Projected schema changes for user management.

--- extra/schema.sql
+++ extra/schema.sql
@@ -2,28 +2,31 @@ CREATE TABLE pastes (
 	paste_id    INTEGER  NOT NULL PRIMARY KEY,
 	created     DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	poster      TEXT     NOT NULL DEFAULT 'Anonymous',
+	user_id     INTEGER  NULL REFERENCES users (user_id),
 	title       TEXT     NOT NULL,
 	body        TEXT     NOT NULL,
 	syntax      TEXT     NOT NULL DEFAULT 'text'
 );
 
 CREATE INDEX ix_paste_created ON pastes (created);
-CREATE INDEX ix_paste_poster ON pastes (poster);
 
 CREATE TABLE comments (
 	comment_id INTEGER  NOT NULL PRIMARY KEY,
 	paste_id   INTEGER  NOT NULL REFERENCES pastes (paste_id),
 	created    DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	poster     TEXT     NOT NULL DEFAULT 'Anonymous',
+	user_id    INTEGER  NULL REFERENCES users (user_id),
 	body       TEXT     NOT NULL
 );
 
 CREATE INDEX ix_comment_paste ON comments (paste_id);
-CREATE INDEX ix_comment_poster ON comments (poster);
 
 CREATE TABLE users (
-	username     TEXT NOT NULL PRIMARY KEY,
-	password     TEXT NULL,
-	display_name TEXT NOT NULL,
-	email        TEXT NULL
+	user_id      INTEGER NOT NULL PRIMARY KEY,
+	username     TEXT    NOT NULL,
+	password     TEXT    NULL,
+	display_name TEXT    NOT NULL,
+	email        TEXT    NULL
 );
+
+CREATE UNIQUE INDEX ux_user_username ON users (username);
