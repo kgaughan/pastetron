067fe3c818ddbf4e18b22ea39495acb5d69786dd
Keith Gaughan <k@stereochro.me>
2013-04-22 13:41:26 +0100

    Pair foreign key references with indexes where necessary.

--- extra/schema.sql
+++ extra/schema.sql
@@ -7,6 +7,7 @@ CREATE TABLE pastes (
 );
 
 CREATE INDEX ix_paste_created ON pastes (created);
+CREATE INDEX ix_paste_creator ON pastes (user_id);
 
 CREATE TABLE chunks (
 	chunk_id    INTEGER  NOT NULL PRIMARY KEY,
@@ -14,6 +15,8 @@ CREATE TABLE chunks (
 	body        TEXT     NOT NULL,
 	syntax      TEXT     NOT NULL DEFAULT 'text'
 );
+
+CREATE INDEX ix_chunk_paste ON chunks (paste_id);
 
 CREATE TABLE comments (
 	comment_id INTEGER  NOT NULL PRIMARY KEY,
@@ -25,6 +28,7 @@ CREATE TABLE comments (
 );
 
 CREATE INDEX ix_comment_paste ON comments (paste_id);
+CREATE INDEX ix_comment_creator ON comments (user_id);
 
 CREATE TABLE users (
 	user_id      INTEGER NOT NULL PRIMARY KEY,
