b27e5fcf42c462d53828c4fb8dab43bd9de2dc43
Keith Gaughan <k@stereochro.me>
2013-08-17 18:21:54 +0100

    Short note on running the application with Paste.

--- pastetron/bootstrap.py
+++ pastetron/bootstrap.py
@@ -10,30 +10,6 @@ import web
 from pastetron import utils, views
 
 
-def configure_db_hook(app, settings):
-    """
-    Ensure each request is done in a properly configured database context.
-    """
-    pool = dbkit.create_pool(sqlite3, 10, settings['db_path'])
-    pool.default_factory = dbkit.dict_set
-
-    def request_processor(handler):
-        """
-        Do anything that needs to be done at the beginning and end of a
-        request here.
-        """
-        with pool.connect():
-            return handler()
-    app.add_processor(request_processor)
-
-
-def configure_authentication(settings):
-    """
-    """
-    views.auth.method = utils.load_object(settings['auth_method'])
-    views.auth.realm = settings['auth_realm']
-
-
 def initialise(app, global_config=None, **settings):
     """
     Initialise a web.py application, returning a WSGI application.
@@ -48,7 +24,25 @@ def initialise(app, global_config=None, **settings):
     settings.setdefault('auth_method', 'pastetron.httpauth:DUMMY')
     settings.setdefault('auth_realm', 'Pastetron')
 
+    # Allow the application access to the configuration.
     web.config.app = web.Storage(**settings)
-    configure_db_hook(app, settings)
-    configure_authentication(settings)
+
+    # Ensure each request is done in a properly configured database context.
+    pool = dbkit.create_pool(sqlite3, 10, settings['db_path'])
+    pool.default_factory = dbkit.dict_set
+
+    def request_processor(handler):
+        """
+        Do anything that needs to be done at the beginning and end of a
+        request here.
+        """
+        with pool.connect():
+            return handler()
+
+    app.add_processor(request_processor)
+
+    # Authentication.
+    views.auth.method = utils.load_object(settings['auth_method'])
+    views.auth.realm = settings['auth_realm']
+
     return app.wsgifunc()
