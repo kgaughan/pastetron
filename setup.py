90e1a8bc477b57219f4b2185becd5d8420b98406
Keith Gaughan <k@stereochro.me>
2013-08-25 05:49:02 +0100

    Simplify setup.py; stricter coverage exclusion.

--- setup.py
+++ setup.py
@@ -1,20 +1,42 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
-from buildkit import *
 
 
 setup(
     name='pastetron',
     version='0.1.0',
     description='A pastebin application',
-    long_description=read('README'),
+    long_description=open('README', 'r').read(),
     url='https://github.com/kgaughan/pastetron/',
     license='MIT',
+
     packages=find_packages(exclude='tests'),
-    zip_safe=False,
-    install_requires=read_requirements('requirements.txt'),
     include_package_data=True,
+    zip_safe=False,
+
+    install_requires=(
+        'dbkit==0.2.2',
+        'mimeparse==0.1.3',
+        'Pygments==1.6rc1',
+        'python-creole==1.0.6',
+        'web.py==0.37',
+    ),
+
+    tests_require=(
+        'nose',
+    ),
+    test_suite='nose.collector',
+
+    extras_require={
+        'dev': (
+            'coverage',
+            'nose',
+            'Paste',
+            'PasteDeploy',
+            'PasteScript',
+        ),
+    },
 
     entry_points={
         'paste.app_factory': (
